154b2a56d0ca81fb85e9900e6e68577a832b11f9
Pulkit Rastogi <pulkit.rastogi@setindia.com>
2025-10-14 16:33:07 +0530

    added version changes

---
+++ mylibrary/src/main/java/com/example/mylibrary/ApiContract.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+/**
+ * Created by Pulkit Rastogi on 14/10/25.
+ **/
+public class ApiContract {
+
+    public static boolean isContractValid(){
+        return true;
+    }
+}
